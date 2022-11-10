3980db7f70569d5f700d294529a2a2279b760bcd
timseymour42 <73562010+timseymour42@users.noreply.github.com>
2022-11-10 18:33:43 -0500

    Revert "added multiple python files to allow layout loading"
    This reverts commit d39d50e358d90373ace0ba5d1f9f35da74b2c754.

--- flask_app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/news")
-def news():
-    return render_template('news.html')
--- server.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-
-server = Flask(__name__)
-
-@server.route('/')
-def server_root():
-    return render_template('index.html')
